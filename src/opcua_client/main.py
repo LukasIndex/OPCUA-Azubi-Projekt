2721981e1b52cf588974c5fe1af5fd1ca23b267b
NoahIndex <noah.lienerth@index-group.com>
2026-06-16 08:38:16 +0200

    Added events subscription

--- src/opcua_client/main.py
+++ src/opcua_client/main.py
@@ -1,23 +1,31 @@
 import asyncio
-import opcua_client.config as config
-from opcua_client.cli import abfrage_wert, abfrage_uname, abfrage_server
-from opcua_client.opcua_client import OpcUaClient
 import getpass
 import argparse
+import opcua_client.config as config
+#  from opcua_client.console import open_window (Out of Scope)
+from opcua_client.cli import abfrage_wert, abfrage_uname, abfrage_server
+from opcua_client.opcua_client import OpcUaClient
+
+
+
+def parser_cli_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--server", help="OPC UA Endpoint")
+    parser.add_argument("--username", help="Username")
+    parser.add_argument("--password", help="Password")
+    #  parser.add_argument("--window", action="store_true", help="Öffnet ein seperates Fenster") (Out of Scope)
+    parser.add_argument("--event", action="store_true", help="Alarmmeldungen der Maschiene")
+    parser.add_argument("--node", help="NodeId")
+    parser.add_argument("--mode", choices=["read", "subscribe"], help="Modus: read oder subscribe")
+    args = parser.parse_args()
+    return args
+
 
 
 async def async_main():
     print("Starte OPC UA Client")
 
-
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--server", help="OPC UA Endpoint")
-    parser.add_argument("--username", help="Username")
-    parser.add_argument("--password", help="Password")
-    parser.add_argument("--node", help="NodeId")
-    parser.add_argument("--mode", choices=["read", "subscribe"], help="Modus: read oder subscribe")
-    args = parser.parse_args()
+    args = parser_cli_arguments()
 
     
     if args.server:
@@ -37,7 +45,7 @@ async def async_main():
     if args.password:
         password = args.password
     else:
-        password = getpass.getpass("Gib dein Passwort ein: ")
+        password = getpass.getpass("Gib dein Passwort ein: ")  # password stays hidden via getpass
 
     client = OpcUaClient(
         config.OPCUA_ENDPOINT,
@@ -50,23 +58,29 @@ async def async_main():
         print("Verbindung aufgebaut")
         print("Angemeldet als:", username)
 
-        while True:
+        if args.event:
+            print("Events Werden Angezeigt:")
+            while client.subscribe_events():
+                await client.subscribe_events()
+                break
+            
 
-            if args.node:
-                config.NODE_TO_READ = args.node
-            else:
-                config.NODE_TO_READ = abfrage_wert()
+        if args.node:
+            config.NODE_TO_READ = args.node
+        else:
+            config.NODE_TO_READ = abfrage_wert()
 
-            print("Node:", config.NODE_TO_READ)
+        print("Node:", config.NODE_TO_READ)
 
           
-            if args.mode:
-                modus = args.mode
-                print("Modus:", modus)
-            else:
-                modus = input("Read oder Subscribe? (R/s): ").strip().lower()  # R in caps to visualize "pre select"
+        if args.mode:
+            modus = args.mode
+            print("Modus:", modus)
+        else:
+            modus = input("Read oder Subscribe? (R/s): ").strip().lower()  # R in caps to visualize "pre select"
 
-           
+        while True:
+
             if modus in ["s", "subscribe"]:
                 await client.subscribe_node(config.NODE_TO_READ)
                 break
--- src/opcua_client/opcua_client.py
+++ src/opcua_client/opcua_client.py
@@ -1,10 +1,38 @@
 import asyncio
+from rich import print  # Import rich print for colored cli output
 from asyncua import Client
 
 
 class SubscriptionHandler:
     def datachange_notification(self, node, val, data):
         print(f"Neuer Wert empfangen: {val}")
+
+class EventSubscriptionHandler:
+    def __init__(self):
+        self.message = None
+
+    def datachange_notification(self, node, val, data):
+        node_id = node.nodeid.to_string()
+
+        if node_id == "ns=2;i=18":
+            self.message = val
+
+        elif node_id == "ns=2;i=19":
+            severity = val
+
+            if self.message is not None:
+                if severity >= 800:
+                    color = "red"
+                elif severity >= 600:
+                    color = "#f47f1f"
+                elif severity >= 400:
+                    color = "#ffb300"
+                elif severity >= 200:
+                    color = "#4cae4f"
+                else:
+                    color = "green"
+
+                print(f"[{color}]Event: {self.message} | Severity: {severity}[/{color}]")
 
 
 class OpcUaClient:
@@ -23,6 +51,25 @@ class OpcUaClient:
 
     async def disconnect(self):
         await self.client.disconnect()
+
+    async def subscribe_events(self):
+        handler = EventSubscriptionHandler()
+
+        print("Event Subscription startet...")
+        await asyncio.sleep(0.3)
+
+        subscription = await self.client.create_subscription(500, handler)
+
+        msg_node = self.client.get_node("ns=2;i=18")
+        sev_node = self.client.get_node("ns=2;i=19")
+
+        await subscription.subscribe_data_change(msg_node)
+        await subscription.subscribe_data_change(sev_node)
+
+        print("Events laufen... (STRG+C zum Beenden)")
+
+        while True:
+            await asyncio.sleep(1)
 
     async def read_node(self, node_id: str):
         node = self.client.get_node(node_id)
