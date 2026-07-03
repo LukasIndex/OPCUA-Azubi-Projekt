a2ec6c8a034df52f5c0d0e4cb7b8f95038e5236f
NoahIndex <noah.lienerth@index-group.com>
2026-07-03 11:30:31 +0200

    changed the event subscription so it only subscribes 1 node

--- src/opcua_client/config.py
+++ src/opcua_client/config.py
@@ -7,5 +7,4 @@ OPC_SESSION_TIMEOUT = 600000
 #  change depending on server
 MACHINE_ID = "ns=0;i=2261"  # machine id node
 BUILD_NUMBER = "ns=0;i=2265"  # build number
-EVENT_MESSAGE = "ns=2;i=18"  # event message node
-EVENT_SEVERITY = "ns=2;i=19"  # event severty node
+EVENT_NODE = "ns=2;i=17"  # node for event subscription
--- src/opcua_client/opcua_client.py
+++ src/opcua_client/opcua_client.py
@@ -1,7 +1,7 @@
 import asyncio
 import opcua_client.config as config
-from rich import print  # Import rich print for colored cli output
 from asyncua import Client
+from rich import print  # Import rich print for colored cli output
 
 
 class SubscriptionHandler:
@@ -9,32 +9,25 @@ class SubscriptionHandler:
         print(f"{val}")
 
 class EventSubscriptionHandler:
-    def __init__(self):
-        self.message = None
+    def event_notification(self, event):
+        try:
+            self.message = event.Message.Text if hasattr(event, "Message") else "Keine Nachricht"
+            severity = event.Severity if hasattr(event, "Severity") else 0
 
-    def datachange_notification(self, node, val, data):
-        node_id = node.nodeid.to_string()
+            if severity >= 800:
+                color = "red"
+            elif severity >= 600:
+                color = "#f47f1f"
+            elif severity >= 400:
+                color = "#ffb300"
+            elif severity >= 200:
+                color = "#4cae4f"
+            else:
+                color = "green"
 
-        if node_id == "ns=2;i=18":
-            self.message = val
-
-        elif node_id == "ns=2;i=19":
-            severity = val
-
-            if self.message is not None:
-                if severity >= 800:
-                    color = "red"
-                elif severity >= 600:
-                    color = "#f47f1f"
-                elif severity >= 400:
-                    color = "#ffb300"
-                elif severity >= 200:
-                    color = "#4cae4f"
-                else:
-                    color = "green"
-
-                print(f"[{color}]Event: {self.message} | Severity: {severity}[/{color}]")
-
+            print(f"[{color}]Event: {self.message} | Severity: {severity}[/{color}]")
+        except Exception as e:
+            print(f"[red]Fehler bei Events: {e}[/red]")            
 
 class OpcUaClient:
     def __init__(self, endpoint: str, username: str, password: str):
@@ -67,16 +60,12 @@ class OpcUaClient:
 
     async def subscribe_events(self):
         handler = EventSubscriptionHandler()
-
         await asyncio.sleep(0.3)
 
         subscription = await self.client.create_subscription(500, handler)
 
-        msg_node = self.client.get_node(config.EVENT_MESSAGE)  # directly reads the nodes for events
-        sev_node = self.client.get_node(config.EVENT_SEVERITY)
-
-        await subscription.subscribe_data_change(msg_node)
-        await subscription.subscribe_data_change(sev_node)
+        event_node = self.client.get_node(config.EVENT_NODE)
+        handle = await subscription.subscribe_events(event_node)
 
         while True:
             await asyncio.sleep(1)
