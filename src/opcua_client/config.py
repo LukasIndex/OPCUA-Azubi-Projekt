12f34b0d7ec85578a970ed5089cbadf892791ec8
NoahIndex <noah.lienerth@index-group.com>
2026-07-13 09:22:54 +0200

    added a few more comments in the opcua_client.py

--- src/opcua_client/config.py
+++ src/opcua_client/config.py
@@ -5,6 +5,6 @@ NODE_TO_READ = None
 TIMEOUT = 60
 OPC_SESSION_TIMEOUT = 600000
 
-#  change depending on server
+#  change depending on server, WONT WORK ON REAL SERVER
 MACHINE_ID = "ns=0;i=2261"  # machine id node
 BUILD_NUMBER = "ns=0;i=2265"  # build number
--- src/opcua_client/opcua_client.py
+++ src/opcua_client/opcua_client.py
@@ -13,7 +13,7 @@ class EventSubscriptionHandler:  # handles events
             self.message = event.Message.Text if hasattr(event, "Message") else "Keine Nachricht"
             severity = event.Severity if hasattr(event, "Severity") else 0
 
-            if severity >= 800:
+            if severity >= 800:  # give color a value depending on severity
                 color = "red"
             elif severity >= 600:
                 color = "#f47f1f"
@@ -28,7 +28,7 @@ class EventSubscriptionHandler:  # handles events
         except Exception as e:
             print(f"[red]Fehler bei Events: {e}[/red]")            
 
-class OpcUaClient:
+class OpcUaClient:  # sets the endpoint, sets the username and password and sets subscription to none
     def __init__(self, endpoint: str, username: str, password: str):
         self.client = Client(url=endpoint)
         self.active_subscription = None
@@ -40,33 +40,33 @@ class OpcUaClient:
         if password:
             self.client.set_password(password)
 
-    async def connect(self):
+    async def connect(self):  # connect to the server
         await self.client.connect()
 
-    async def disconnect(self):
+    async def disconnect(self):  # disconnect from the server
         await self.client.disconnect()
 
-    async def read_node(self, node_id: str):
+    async def read_node(self, node_id: str):  # function that reads the specified node from the server
         node = self.client.get_node(node_id)
         return await node.read_value()
 
-    async def machine_identifiers(self):
+    async def machine_identifiers(self): # WONT WORK ON REAL SERVER, reads the nodes specified in config.py
         try:
-            machine_name = await self.read_node(config.MACHINE_ID)  # reads the node with the machine name
+            machine_name = await self.read_node(config.MACHINE_ID)
             build_number = await self.read_node(config.BUILD_NUMBER)
             print(f"[blue]Maschinen Bezeichnung: {machine_name}, Buildnummer: {build_number} [/blue]")
         except Exception as e:
             print(f"Maschinen Identifizierungen nicht lesbar: {e}")
 
-    async def subscribe_events(self):
+    async def subscribe_events(self):  # creates event subscription and uses the handler
         handler = EventSubscriptionHandler()
         await asyncio.sleep(0.3)
 
         self.active_subscription = await self.client.create_subscription(500, handler)
-        event_node = self.client.get_node(config.EVENT_NODE)
+        event_node = self.client.get_node(config.EVENT_NODE)  # get_node to define which node to subscribe
         await self.active_subscription.subscribe_events(event_node)
 
-    async def subscribe_node(self):
+    async def subscribe_node(self):  # creates node subscription
         handler = SubscriptionHandler()
         await asyncio.sleep(0.3)
 
